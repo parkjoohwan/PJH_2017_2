fdf07252cd628d21dde572621867663ac41fd19b
JOO <jookid94@gmail.com>
2017-09-26 18:27:26 +0900

    0926

---
+++ 모바일프로그래밍/강의연습/0926ex.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0
+Transitional//EN'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
+<html xmlns='htp://www.w3.org/19/xhtml'>
+<meta http-equiv='Content-Type' content='text/html; chaset=utf-8'/>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		<h1>DOCTYPE 연습</h1>
+		<p>XHTML1.0 Strict 범주를 사용한 페이지입니다.</p>
+
+		<p>문단1</p><p>문단2</p>
+		<p><em>강조1</em><em>강조2</em></p>
+		<h1>첫번째로 중요한 제목</h1>
+		<h2>두번째로 중요한 내용</h2>
+		<h3>세번째로 중요한 내용</h3>
+		<h4>네번째로 중요한 내용</h4>
+		<h5>다섯번째로 중요한 내용</h5>
+		<h6>여섯번째로 중요한 내용</h6>
+		<!-- 반드시 순차적으로 적용해야함, 중간을 건너뛰면 적용되지않음 -->
+		<p>문단1</p><p>문단2</p>
+		<p><em>강조1</em><em>강조2</em></p>
+		<div style='background-color: #008000'>
+		<h1>제목</h1>
+		<p>문장</p>
+		</div>
+		<p>
+		<span style='background-color: #FFFF00'><em>강조</em>한 문자 및
+		<strong>더 강조</strong></span>한 문자
+		</p>
+
+		<header>
+			<h1>HTML 앱 만들기 스터디</h1>
+		</header>
+		<nav>
+			<a href='what.html'>HTML5는 무엇인가?</a>
+			<a href='why.html'>HTML5는 왜 사용해야 하는가?</a>
+			<a href='how.html'>HTML5는 어떻게 사용할 수 있는가?</a>
+		</nav>
+
+		<article>
+			<section>
+				<h2>가장 최신 버전의 HTML</h2>
+				<p>HTML5는 미래의 웹 황금 표준이다.</p>
+			</section>
+			<section>
+				<h2>HTML5 앱이란 무엇인가?</h2>
+				<p>HTML5를 사용해서 앱과 동일한 경험을 제공하는 웹을 말한다.</p>
+			</section>
+		</article>
+
+		<footer> Copyright &copy; 2011 ALL rights reserved.</footer>
+	</body>
+</html>
